4988816c6408450223f18e19f5b3898a63d685ef
Aaron Rieder <737456+AARtwo@users.noreply.github.com>
2025-01-08 10:51:18 -0500

    Remove HbmSVManager

--- lynx-vendor.mk
+++ lynx-vendor.mk
@@ -478,7 +478,6 @@ PRODUCT_PACKAGES += \
     libmediaadaptor \
     com.google.edgetpu_app_service-V3-ndk \
     com.google.edgetpu_vendor_service-V2-ndk \
-    libhbmsvmanager_jni \
     VZWAPNLib \
     AppDirectedSMSService \
     CarrierSettings_signed_49.0.645958608 \
